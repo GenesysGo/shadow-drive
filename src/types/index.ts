97decd0736672446deca433f16325f4f7e3a3c3a
Alice <alize_in_cryptoland@protonmail.com>
2022-05-28 16:59:42 +0200

    undo

--- src/types/index.ts
+++ src/types/index.ts
@@ -17,7 +17,7 @@ export type ShadowBatchUploadResponse = {
   fileName: string;
   status: string;
   location: string;
-  transaction_signature: string | null;
+  transaction_signature?: string;
 };
 
 export type StorageAccountResponse = {
