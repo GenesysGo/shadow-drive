a48f5cae94e6956fc72ebd3a45dad5ffbe087359
khalilnafis <khalilnafis@gmail.com>
2022-07-07 13:59:24 -0700

    update tests

--- tests/shadow-drive.spec.ts
+++ tests/shadow-drive.spec.ts
@@ -29,7 +29,7 @@ describe("shadow-drive v2 sdk testing", () => {
   beforeAll(async () => {
     //Must be on official devnet endpoint for SOL airdrops to work
     connection = new anchor.web3.Connection(
-      "https://api.devnet.solana.com/",
+      "https:/api.devnet.solana.com/",
       "max"
     );
   });
@@ -111,94 +111,100 @@ describe("shadow-drive v2 sdk testing", () => {
       "100MB",
       "v2"
     );
-    let accountKey = new anchor.web3.PublicKey(res.shdw_bucket);
-    const immutRes = await drive.makeStorageImmutable(accountKey, "v2");
+    let newAccount = new anchor.web3.PublicKey(res.shdw_bucket);
+    const immutRes = await drive.makeStorageImmutable(newAccount, "v2");
     expect.objectContaining<ShadowDriveResponse>(immutRes);
   });
-  describe("shadow-drive v1 sdk testing", () => {
-    let connection: anchor.web3.Connection,
-      drive: ShdwDrive,
-      balance: number,
-      shdwBalance: number | null,
-      user: anchor.web3.Keypair,
-      userATA: anchor.web3.PublicKey,
-      wallet: anchor.Wallet,
-      accountKey: anchor.web3.PublicKey,
-      uploadResponse: ShadowUploadResponse;
-    beforeAll(async () => {
-      //Must be on official devnet endpoint for SOL airdrops to work
-      connection = new anchor.web3.Connection(
-        "https://api.devnet.solana.com/",
-        "max"
-      );
-    });
-    it("funds a new wallet for tests", async () => {
-      [user, userATA] = await getFundedUser(connection);
-      wallet = new anchor.Wallet(user);
+  it("creates a v1 shadow drive storage account and migrates to v2", async () => {
+    const res = await drive?.createStorageAccount(
+      "migrate-account",
+      "100MB",
+      "v1"
+    );
+    const migrateRes = await drive.migrate(
+      new anchor.web3.PublicKey(res.shdw_bucket)
+    );
+    expect.objectContaining<ShadowDriveResponse>(migrateRes);
+  });
+});
+describe("shadow-drive v1 sdk testing", () => {
+  let connection: anchor.web3.Connection,
+    drive: ShdwDrive,
+    balance: number,
+    shdwBalance: number | null,
+    user: anchor.web3.Keypair,
+    userATA: anchor.web3.PublicKey,
+    wallet: anchor.Wallet,
+    accountKey: anchor.web3.PublicKey,
+    uploadResponse: ShadowUploadResponse;
+  beforeAll(async () => {
+    //Must be on official devnet endpoint for SOL airdrops to work
+    connection = new anchor.web3.Connection("http://localhost:8899/", "max");
+  });
+  it("funds a new wallet for tests", async () => {
+    [user, userATA] = await getFundedUser(connection);
+    wallet = new anchor.Wallet(user);
 
-      try {
-        balance = await connection.getBalance(user.publicKey);
-        shdwBalance = (await connection.getTokenAccountBalance(userATA)).value
-          .uiAmount;
-      } catch (e) {
-        console.log(e);
-      }
-      expect(balance).toBeGreaterThanOrEqual(
-        0.8 * anchor.web3.LAMPORTS_PER_SOL
-      );
-      expect(shdwBalance).toBeGreaterThanOrEqual(10);
-    });
-    it("initializes an sdk client", async () => {
-      drive = await new ShdwDrive(connection, wallet).init();
-      expect.objectContaining<ShdwDrive>(drive);
-    });
-    it("creates a v1 shadow drive storage account", async () => {
-      const res = await drive?.createStorageAccount(
-        "test-account",
-        "500MB",
-        "v1"
-      );
-      accountKey = new anchor.web3.PublicKey(res.shdw_bucket);
-      expect.objectContaining<CreateStorageResponse>(res);
-    });
-    it("add storage on a new v1 storage account", async () => {
-      const addRes = await drive.addStorage(accountKey, "10MB", "v1");
-      expect.objectContaining<ShadowDriveResponse>(addRes);
-    });
-    it("reduce reduce storage on a v1 storage account", async () => {
-      const reduceRes = await drive.reduceStorage(accountKey, "12MB", "v1");
-      expect.objectContaining<ShadowDriveResponse>(reduceRes);
-    });
-    it("fails to create shadow drive storage account with invalid size", () => {
-      expect(
-        drive.createStorageAccount("my-failed-account", "1PB", "v1")
-      ).rejects.toThrow(Error);
-    });
-    it("uploads a file to shadowDrive", async () => {
-      let file = {
-        name: "hey.txt",
-        file: fs.readFileSync(path.join(__dirname, "/test-files/hey.txt")),
-      };
-      uploadResponse = await drive.uploadFile(accountKey, file, "v1");
-      expect.objectContaining<ShadowUploadResponse>(uploadResponse);
-    });
-    it("edits file on shadowDrive", async () => {
-      let file = {
-        name: "hey.txt",
-        file: fs.readFileSync(path.join(__dirname, "/test-files/hey-edit.txt")),
-      };
-      console.log(uploadResponse.finalized_locations[0]);
-      const editRes = await drive.editFile(
-        accountKey,
-        uploadResponse.finalized_locations[0],
-        file,
-        "v1"
-      );
-      expect.objectContaining<ShadowUploadResponse>(editRes);
-    });
-    it("deletes a shadow drive storage account", async () => {
-      const delRes = await drive.deleteStorageAccount(accountKey, "v1");
-      expect.objectContaining<ShadowDriveResponse>(delRes);
-    });
+    try {
+      balance = await connection.getBalance(user.publicKey);
+      shdwBalance = (await connection.getTokenAccountBalance(userATA)).value
+        .uiAmount;
+    } catch (e) {
+      console.log(e);
+    }
+    expect(balance).toBeGreaterThanOrEqual(0.8 * anchor.web3.LAMPORTS_PER_SOL);
+    expect(shdwBalance).toBeGreaterThanOrEqual(10);
+  });
+  it("initializes an sdk client", async () => {
+    drive = await new ShdwDrive(connection, wallet).init();
+    expect.objectContaining<ShdwDrive>(drive);
+  });
+  it("creates a v1 shadow drive storage account", async () => {
+    const res = await drive?.createStorageAccount(
+      "test-account",
+      "500MB",
+      "v1"
+    );
+    accountKey = new anchor.web3.PublicKey(res.shdw_bucket);
+    expect.objectContaining<CreateStorageResponse>(res);
+  });
+  it("add storage on a new v1 storage account", async () => {
+    const addRes = await drive.addStorage(accountKey, "10MB", "v1");
+    expect.objectContaining<ShadowDriveResponse>(addRes);
+  });
+  it("reduce reduce storage on a v1 storage account", async () => {
+    const reduceRes = await drive.reduceStorage(accountKey, "12MB", "v1");
+    expect.objectContaining<ShadowDriveResponse>(reduceRes);
+  });
+  it("fails to create shadow drive storage account with invalid size", () => {
+    expect(
+      drive.createStorageAccount("my-failed-account", "1PB", "v1")
+    ).rejects.toThrow(Error);
+  });
+  it("uploads a file to shadowDrive", async () => {
+    let file = {
+      name: "hey.txt",
+      file: fs.readFileSync(path.join(__dirname, "/test-files/hey.txt")),
+    };
+    uploadResponse = await drive.uploadFile(accountKey, file, "v1");
+    expect.objectContaining<ShadowUploadResponse>(uploadResponse);
+  });
+  it("edits file on shadowDrive", async () => {
+    let file = {
+      name: "hey.txt",
+      file: fs.readFileSync(path.join(__dirname, "/test-files/hey-edit.txt")),
+    };
+    console.log(uploadResponse.finalized_locations[0]);
+    const editRes = await drive.editFile(
+      accountKey,
+      uploadResponse.finalized_locations[0],
+      file,
+      "v1"
+    );
+    expect.objectContaining<ShadowUploadResponse>(editRes);
+  });
+  it("deletes a shadow drive storage account", async () => {
+    const delRes = await drive.deleteStorageAccount(accountKey, "v1");
+    expect.objectContaining<ShadowDriveResponse>(delRes);
   });
 });
