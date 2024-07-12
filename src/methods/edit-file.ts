ecad37969be7122f767e09be4667b33b288380d6
Tracy <31976538+tracy-codes@users.noreply.github.com>
2024-07-12 10:36:54 -0700

    feat: refactors edit-file function for more stable editing
    edit-file function signature has changed by removing the url input variable and the return is now Promise<ShadowUploadResponse>

--- src/methods/edit-file.ts
+++ src/methods/edit-file.ts
@@ -1,32 +1,27 @@
 import { web3 } from "@coral-xyz/anchor";
 import { isBrowser, SHDW_DRIVE_ENDPOINT } from "../utils/common";
 import crypto from "crypto";
-import { ShadowFile, ShadowEditResponse } from "../types";
 import fetch from "cross-fetch";
+import { ShadowFile, ShadowUploadResponse } from "../types";
 import NodeFormData from "form-data";
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 import nacl from "tweetnacl";
-import { UserInfo } from "../types/accounts";
 /**
  *
- * @param {web3.PublicKey} key - Publickey of Storage Account
- * @param {string} url - URL of existing file
+ * @param {web3.PublicKey} key - Publickey of Storage Account.
  * @param {File | ShadowFile} data - File or ShadowFile object, file extensions should be included in the name property of ShadowFiles.
- * @returns {ShadowEditResponse} - File location
+ * @returns {ShadowUploadResponse} File location and transaction signature.
  */
-
 export default async function editFile(
-  key: web3.PublicKey,
-  url: string,
+  key: web3.PublicKey,  
   data: File | ShadowFile
-): Promise<ShadowEditResponse> {
+): Promise<ShadowUploadResponse> {
   let fileErrors = [];
-  let fileBuffer: Buffer | ArrayBuffer;
+  let fileBuffer: Buffer;
   let form;
   let file;
 
   if (!isBrowser) {
-    console.log(`not browser`);
     data = data as ShadowFile;
     form = new NodeFormData();
     file = data.file;
@@ -41,70 +36,60 @@ export default async function editFile(
 
   if (fileBuffer.byteLength > 1_073_741_824 * 1) {
     fileErrors.push({
-      file: file,
+      file: data.name,
       erorr: "Exceeds the 1GB limit.",
     });
   }
+  /**
+   *
+   * Users must remember to include the file extension when uploading from Node.
+   *
+   */
+  //   if (!isBrowser && data.name.lastIndexOf(".") == -1) {
+  //     fileErrors.push({
+  //       file: data.name,
+  //       error: "File extension must be included.",
+  //     });
+  //   }
   if (fileErrors.length) {
     return Promise.reject(fileErrors);
   }
-  const userInfoAccount = await UserInfo.fetch(this.connection, this.userInfo);
-  if (userInfoAccount === null) {
+  if (!this.userInfo) {
     return Promise.reject(
-      new Error(
-        "You have not created a storage account on Shadow Drive yet. Please see the 'create-storage-account' command to get started."
-      )
+      "You have not created a storage account on Shadow Drive yet. Please see the 'create-storage-account' command to get started."
     );
   }
-  let existingFileData, fileDataResponse;
-  try {
-    existingFileData = await fetch(`${SHDW_DRIVE_ENDPOINT}/get-object-data`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        location: url,
-      }),
-    });
-    fileDataResponse = await existingFileData.json();
-  } catch (e) {
-    return Promise.reject(new Error(e.message));
-  }
-  const fileOwnerOnChain = new web3.PublicKey(
-    fileDataResponse.file_data["owner-account-pubkey"]
-  );
-  if (!fileOwnerOnChain.equals(this.wallet.publicKey)) {
-    return Promise.reject(new Error("Permission denied: Not file owner"));
-  }
   const fileHashSum = crypto.createHash("sha256");
+  const fileNameHashSum = crypto.createHash("sha256");
   fileHashSum.update(
     Buffer.isBuffer(fileBuffer) ? fileBuffer : Buffer.from(fileBuffer)
   );
-  const fileHash = fileHashSum.digest("hex");
+  fileNameHashSum.update(data.name);
 
+  const fileNameHash = fileNameHashSum.digest("hex");
   try {
-    const msg = Buffer.from(
-      `Shadow Drive Signed Message:\n StorageAccount: ${key}\nFile to edit: ${data.name}\nNew file hash: ${fileHash}`
+    const msg = new TextEncoder().encode(
+      `Shadow Drive Signed Message:\nStorage Account: ${key}\nUpload files with hash: ${fileNameHash}`
     );
-    let msgSig;
+    let msgSig: Uint8Array;
     if (!this.wallet.signMessage) {
       msgSig = nacl.sign.detached(msg, this.wallet.payer.secretKey);
     } else {
       msgSig = await this.wallet.signMessage(msg);
     }
     const encodedMsg = bs58.encode(msgSig);
+    form.append("fileNames", data.name);
     form.append("message", encodedMsg);
-    form.append("signer", this.wallet.publicKey.toString());
     form.append("storage_account", key.toString());
-    form.append("url", url);
+    form.append("signer", this.wallet.publicKey.toString());
+    form.append("overwrite", "true");
   } catch (e) {
     return Promise.reject(new Error(e.message));
   }
   try {
     const controller = new AbortController();
     const timeoutId = setTimeout(() => controller.abort(), 7200000);
-    const uploadResponse = await fetch(`${SHDW_DRIVE_ENDPOINT}/edit`, {
+    const uploadResponse = await fetch(`${SHDW_DRIVE_ENDPOINT}/upload`, {
       method: "POST",
       //@ts-ignore
       body: form,
@@ -116,7 +101,7 @@ export default async function editFile(
 				  Server response status message: ${(await uploadResponse.json()).error}`)
       );
     }
-    const responseJson: ShadowEditResponse = await uploadResponse.json();
+    const responseJson = await uploadResponse.json();
     return Promise.resolve(responseJson);
   } catch (e) {
     return Promise.reject(new Error(e.message));
